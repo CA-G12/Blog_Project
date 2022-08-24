0a2926a1e8f52f33be75736553387e5b5f535cfb
sam96B <same-96@hotmial.com>
2022-08-24 13:16:48 +0300

    handle setuser router

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,5 +1,4 @@
-const router = require('express').Router();
+const users = require('./users');
+const posts = require('./posts');
 
-// main endpoints
-
-module.exports = router;
+module.exports = { users, posts };
